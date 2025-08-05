b0dc4dbc099738fb4405909a189ef376833bfea9
Shivam Roy <devopsroy9@gmail.com>
2025-08-06 03:58:37 +0530

    Update Home.jsx

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -32,7 +32,7 @@ function Home() {
     formData.append("file", selectedFile);
     try {
       const response = await axios.post(
-        "http://localhost:3000/convertfile",
+        "https://word-to-pdf-converter-eight.vercel.app/convertfile",
         formData,
         {
           responseType: "blob",
