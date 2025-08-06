acfa82ff9456d4694e843e5ced7a839e9d598f88
Shivam Roy <devopsroy9@gmail.com>
2025-08-07 03:32:28 +0530

    updated front

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 // configure of CORS
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://word-to-pdf-orpin.vercel.app",
     methods: ["GET", "POST", "OPTIONS", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
 };
