d17f54caf335663276a0989c6d8bd24e855c17b9
Shivam Roy <devopsroy9@gmail.com>
2025-08-06 23:33:21 +0530

    Your commit message here

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ const docxToPDF = require("docx-pdf");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
@@ -20,7 +20,7 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-app.post("/convertFile", upload.single("file"), (req, res, next) => {
+app.post("/convertfile", upload.single("file"), (req, res, next) => {
     try {
         if (!req.file) {
             return res.status(400).json({
